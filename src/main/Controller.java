571725bcb483fb85cc3ac948fdc21a1dc522e870
Conor <Conorbaker52@gmail.com>
2021-05-20 17:49:29 +0100

    Continued Controller class and features as well as appropriate tests

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -4,27 +4,42 @@ import java.util.ArrayList;
 
 public class Controller {
 
-    public Rubric createRubric(String topic, ArrayList<Criterion> criteria){
-        Rubric rubric = new Rubric(topic,criteria);
+    public Rubric createRubric(String topic, ArrayList<Criterion> criteria) {
+        Rubric rubric = new Rubric(topic, criteria);
         return rubric;
     }
 
-    public void addCriterion(Rubric rubric, Criterion criteria){
+    public void addCriterion(Rubric rubric, Criterion criteria) {
         rubric.addCriteria(criteria);
     }
 
-    public ArrayList<Rubric> getAllRubrics(ArrayList<Rubric> rubrics){
+    public ArrayList<Rubric> getAllRubrics(ArrayList<Rubric> rubrics) {
         return rubrics;
     }
 
-    public Rubric getOneRubric(ArrayList<Rubric> rubrics, String name){
-        Rubric rubric = new Rubric("test",new ArrayList<>());
-        for(int x = 0; x < rubrics.size(); x++){
-            if(rubrics.get(x).getTopic() == name){
+    public Rubric getOneRubric(ArrayList<Rubric> rubrics, String name) {
+        Rubric rubric = new Rubric("test", new ArrayList<>());
+        for (int x = 0; x < rubrics.size(); x++) {
+            if (rubrics.get(x).getTopic() == name) {
                 rubric = rubrics.get(x);
             }
         }
         return rubric;
     }
 
+    public Student newStudent(String name) {
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        Student student = new Student(name, rubrics);
+        return student;
+    }
+
+    public void addRubric(Student student, Rubric rubric) {
+        student.addRubrics(rubric);
+    }
+
+    public void setGrade(Student student, String topic, String criterion, int grade) {
+        Rubric rubric = student.getARubric(topic);
+        Criterion criteria = rubric.getACriterion(rubric, criterion);
+        criteria.setScore(grade);
+    }
 }
