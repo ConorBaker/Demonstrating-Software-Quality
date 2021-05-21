dcc7f6de74086cad1baa924e8693f91051a1df9f
Conor <Conorbaker52@gmail.com>
2021-05-21 12:50:42 +0100

    Added Controller class and suitable tests

--- src/test/ControllerTest.java
+++ src/test/ControllerTest.java
@@ -54,7 +54,6 @@ public class ControllerTest {
         rubrics.add(rubric);
         rubrics.add(rubric2);
         rubrics.add(rubric3);
-
         assertEquals(rubrics,unitTest.getAllRubrics(rubrics));
 
     }
@@ -136,5 +135,308 @@ public class ControllerTest {
                 student2.getARubric("Maths",student2.getRubrics()).getACriterion(student2.getARubric("Maths",student2.getRubrics()).getCriteria(),"Design").getScore());
 
     }
+
+    @Test
+    public void testAvgRubric(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+
+        criteria.add(Design);
+        criteria.add(Implementation);
+        criteria.add(Testing);
+        criteria.add(Documentation);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student,"Maths","Implementation",3);
+        unitTest.setGrade(student,"Maths","Testing",1);
+        unitTest.setGrade(student,"Maths","Documentation",2);
+
+        assertEquals(2,unitTest.calculateAverageRubric(students, "Maths"));
+
+    }
+
+    @Test
+    public void TestSDRubric(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+
+        criteria.add(Design);
+        criteria.add(Implementation);
+        criteria.add(Testing);
+        criteria.add(Documentation);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student,"Maths","Implementation",3);
+        unitTest.setGrade(student,"Maths","Testing",1);
+        unitTest.setGrade(student,"Maths","Documentation",2);
+
+        assertEquals("0.8408964152537146",String.valueOf(unitTest.calculateSDRubric(students, "Maths")));
+
+    }
+
+    @Test
+    public void TestMinRubric(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+
+        criteria.add(Design);
+        criteria.add(Implementation);
+        criteria.add(Testing);
+        criteria.add(Documentation);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student,"Maths","Implementation",3);
+        unitTest.setGrade(student,"Maths","Testing",1);
+        unitTest.setGrade(student,"Maths","Documentation",2);
+
+        assertEquals(1,unitTest.calculateMinRubric(students,"Maths"));
+
+    }
+
+    @Test
+    public void TestMaxRubric(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+
+        criteria.add(Design);
+        criteria.add(Implementation);
+        criteria.add(Testing);
+        criteria.add(Documentation);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student,"Maths","Implementation",3);
+        unitTest.setGrade(student,"Maths","Testing",1);
+        unitTest.setGrade(student,"Maths","Documentation",2);
+
+        assertEquals(3,unitTest.calculateMaxRubric(students,"Maths"));
+    }
+
+    @Test
+    public void testAvgRubricCitation(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Rubric> rubrics2 = new ArrayList<>();
+        ArrayList<Rubric> rubrics3 = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        Criterion Design2= new Criterion("Design",0);
+
+        Criterion Design3 = new Criterion("Design",0);
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+        criteria.add(Design);
+
+        ArrayList<Criterion> criteria2 = new ArrayList<>();
+        criteria2.add(Design2);
+
+        ArrayList<Criterion> criteria3 = new ArrayList<>();
+        criteria3.add(Design3);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Rubric rubric2 = new Rubric("Maths",criteria2);
+        rubrics2.add(rubric2);
+
+        Rubric rubric3 = new Rubric("Maths",criteria3);
+        rubrics3.add(rubric3);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        Student student2 = new Student("Conor", rubrics2);
+        students.add(student2);
+
+        Student student3 = new Student("Oisin", rubrics3);
+        students.add(student3);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student2,"Maths","Design",3);
+        unitTest.setGrade(student3,"Maths","Design",1);
+
+
+        assertEquals(2,unitTest.calculateAverageRubricAndCriterion(students, "Maths", "Design"));
+
+    }
+
+    @Test
+    public void TestSDRubricCitation(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Rubric> rubrics2 = new ArrayList<>();
+        ArrayList<Rubric> rubrics3 = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        Criterion Design2= new Criterion("Design",0);
+
+        Criterion Design3 = new Criterion("Design",0);
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+        criteria.add(Design);
+
+        ArrayList<Criterion> criteria2 = new ArrayList<>();
+        criteria2.add(Design2);
+
+        ArrayList<Criterion> criteria3 = new ArrayList<>();
+        criteria3.add(Design3);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Rubric rubric2 = new Rubric("Maths",criteria2);
+        rubrics2.add(rubric2);
+
+        Rubric rubric3 = new Rubric("Maths",criteria3);
+        rubrics3.add(rubric3);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        Student student2 = new Student("Conor", rubrics2);
+        students.add(student2);
+
+        Student student3 = new Student("Oisin", rubrics3);
+        students.add(student3);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student2,"Maths","Design",3);
+        unitTest.setGrade(student3,"Maths","Design",1);
+
+        assertEquals("0.9036020036098449",String.valueOf(unitTest.calculateSDRubricAndCriterion(students, "Maths", "Design")));
+
+    }
+
+    @Test
+    public void TestMinRubricCitation(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Rubric> rubrics2 = new ArrayList<>();
+        ArrayList<Rubric> rubrics3 = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        Criterion Design2= new Criterion("Design",0);
+
+        Criterion Design3 = new Criterion("Design",0);
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+        criteria.add(Design);
+
+        ArrayList<Criterion> criteria2 = new ArrayList<>();
+        criteria2.add(Design2);
+
+        ArrayList<Criterion> criteria3 = new ArrayList<>();
+        criteria3.add(Design3);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Rubric rubric2 = new Rubric("Maths",criteria2);
+        rubrics2.add(rubric2);
+
+        Rubric rubric3 = new Rubric("Maths",criteria3);
+        rubrics3.add(rubric3);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        Student student2 = new Student("Conor", rubrics2);
+        students.add(student2);
+
+        Student student3 = new Student("Oisin", rubrics3);
+        students.add(student3);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student2,"Maths","Design",3);
+        unitTest.setGrade(student3,"Maths","Design",1);
+
+        assertEquals(1,unitTest.calculateMinRubricAndCriterion(students,"Maths","Design"));
+
+    }
+
+    @Test
+    public void TestMaxRubricCitation(){
+        ArrayList<Rubric> rubrics = new ArrayList<>();
+        ArrayList<Rubric> rubrics2 = new ArrayList<>();
+        ArrayList<Rubric> rubrics3 = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Controller unitTest = new Controller();
+
+        Criterion Design2= new Criterion("Design",0);
+
+        Criterion Design3 = new Criterion("Design",0);
+
+        ArrayList<Criterion> criteria = new ArrayList<>();
+        criteria.add(Design);
+
+        ArrayList<Criterion> criteria2 = new ArrayList<>();
+        criteria2.add(Design2);
+
+        ArrayList<Criterion> criteria3 = new ArrayList<>();
+        criteria3.add(Design3);
+
+        Rubric rubric = new Rubric("Maths",criteria);
+        rubrics.add(rubric);
+
+        Rubric rubric2 = new Rubric("Maths",criteria2);
+        rubrics2.add(rubric2);
+
+        Rubric rubric3 = new Rubric("Maths",criteria3);
+        rubrics3.add(rubric3);
+
+        Student student = new Student("Una", rubrics);
+        students.add(student);
+
+        Student student2 = new Student("Conor", rubrics2);
+        students.add(student2);
+
+        Student student3 = new Student("Oisin", rubrics3);
+        students.add(student3);
+
+        unitTest.setGrade(student,"Maths","Design",2);
+        unitTest.setGrade(student2,"Maths","Design",3);
+        unitTest.setGrade(student3,"Maths","Design",1);
+
+        assertEquals(3,unitTest.calculateMaxRubricAndCriterion(students,"Maths","Design"));
+    }
+
+
+
+
 
 }
